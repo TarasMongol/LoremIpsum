bb2ff3203a5c7a4aa4abf00bb633bf8f6f138e9e
kovaan15 <kovaan15@29-2>
2025-09-05 11:38:11 +0300

    Add project files.

---
+++ Lorem Ipsum/FileName.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Point // создание своего ТИПА!!!
+{
+private: // реализация объекта
+    int x; // поле объекта
+    int y; // поле объекта
+    char s; // поле объекта
+
+public: // интерфейс объекта
+    void Print() // метод класса 
+    {
+        cout << s << "-> X: " << x << "\tY: " << y << endl;
+    }
+    void Init(char t, int _x, int _y) // метод класса 
+    {
+        s = t;
+        x = _x;
+        y = _y;
+    }
+};
+int main() // клент
+{
+    Point a, b;
+    //cout << sizeof(a) << endl; // 12
+
+    a.Init('A', 10, -20);
+    a.Print();
+
+    b.Init('B', 5, 16);
+    b.Print();
+}
